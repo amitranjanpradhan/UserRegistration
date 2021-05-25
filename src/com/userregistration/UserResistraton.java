b91f74062dc5ff37a6a5bf3b0a38b281dbcb459d
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-25 20:02:55 +0530

    [Amit] Added Valid First Name_UC1

--- src/com/userregistration/UserResistraton.java
+++ src/com/userregistration/UserResistraton.java
@@ -1,8 +1,27 @@
 package com.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserResistraton {
+	public static boolean usernameValidation(String name) 
+	{
+		String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+		Pattern p = Pattern.compile(regex);
+		if (name == null) 
+		{
+			return false;
+			System.out.println("Invalid Name Entered!!! ");
+		}
+		 Matcher m = p.matcher(name);
+		 return m.matches();	 
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome To User Registration");
+		Scanner input = new Scanner(System.in); 
+		System.out.print("Enter Your First Name : ");
+		String firstname = input.nextLine();	 
+	    System.out.println(usernameValidation(firstname));
 	}
 
 }
