733093f20afaaa7da2e1dc5da9dbe14654b1ef59
amitranjan <amitranjanpradhan456@gmail.com>
2021-06-01 00:34:41 +0530

    Added UC13-Lambda Function to UserRegistration

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,70 +1,77 @@
 package com.userregistration;
+import java.util.Scanner;
 import java.util.regex.*;
-public class UserRegistration extends Throwable {
+/**
+ * for lambda implementation i have refactored the code.
+ * added Functional interface.
+ * i have taken Iuser interface and taken one abstract method.
+ */
+@FunctionalInterface
+interface IUser{
+    void validateUser(String pattern, String input);
+}
+
+public class UserRegistration {
     private static boolean check;
-    private String firstName,lastName,email,phoneNumber,password;
+    private String email;
+    private String firstName, lastName, phoneNumber, password;
     /**
-     * This checkFirstname method will check the user input according to the pattern.
-     * for this operation i have imported regex module.
+     * Here i have taken all the patterns which i have to compare with the userinputs.
      */
-    public static boolean checkFirstName(String firstName) throws UserRegistrationException{
-        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
-        if (!check) {
-            return false;
-        }
-        else {
-            return true;
-        }
+    private final String FIRSTANDLASTNAME="[A-Z]{1}[a-z]{2,}";
+    private final String EMAIL="^[a-zA-Z0-9]+(?:\\+*-*.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,}+)*$";
+    private final String PASSWORD="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]?){8,}.*$";
+    private final String PHONENUMBER="^[0-9]{1,3} [0-9]{10}$";
+    private final Scanner SCAN=new Scanner(System.in);
+
+    /**
+     * Added an Uservalidate method.
+     * it'll perform user validation operations.
+     * Added exception Handling.
+     */
+    public void userValidate(){
+        IUser userRegistration=(checkPattern, givenInput)->{
+            try{
+                if (givenInput==null){
+                    throw new UserRegistrationException("Enter valid Input ");
+                }else if (!(Pattern.matches(checkPattern, givenInput))){
+                    throw new UserRegistrationException("Enter valid input ");
+                }
+                System.out.println(givenInput+" is a valid input.");
+            } catch (UserRegistrationException e) {
+                String printMessage=e.errorMessage;
+                System.out.println(printMessage);
+                userValidate();
+            }
+        };
+        System.out.println("Enter Your Firstname: ");
+        firstName=SCAN.nextLine();
+        userRegistration.validateUser(FIRSTANDLASTNAME,firstName);
+
+        System.out.println("Enter Your Lastname: ");
+        lastName=SCAN.nextLine();
+        userRegistration.validateUser(FIRSTANDLASTNAME,lastName);
+
+        System.out.println("Enter Your Email-Id: ");
+        email=SCAN.nextLine();
+        userRegistration.validateUser(EMAIL,email);
+
+        System.out.println("Enter Your Phone Number: ");
+        phoneNumber=SCAN.nextLine();
+        userRegistration.validateUser(PHONENUMBER,phoneNumber);
+
+        System.out.println("Enter Your Password: ");
+        password=SCAN.nextLine();
+        userRegistration.validateUser(PASSWORD,password);
     }
+
     /**
-     * This checkLastname method will check the user input according to the pattern.
+     * Creating objects for every method in the UserRegistration.
+     * it will call the method and proceed the opration.
+     * @param args
      */
-    public static boolean checkLastName(String lastName) throws UserRegistrationException{
-        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
-        if (!check) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-    /**
-     * Added checkEmail method to match the email ids according to the given pattern.
-     */
-    public static boolean checkEmail(String email) throws IllegalArgumentException{
-        check = Pattern.compile("^[a-zA-Z0-9]+(?:\\+*-*.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,}+)*$").matcher(email).matches();
-        if (!check) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-    /**
-     * This checkPhonenumber method will check the user input(Mobile Number) according to the pattern.
-     */
-    public static boolean checkPhoneNumber(String phoneNumber) throws UserRegistrationException{
-        check = Pattern.compile("^[0-9]{1,3} [0-9]{10}$").matcher(phoneNumber).matches();
-        if (!check) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-    /**
-     * Password must be of 8 characters.
-     * Have one Uppercase Letter.
-     * Have a Special Character(Eg-@$^)"
-     * Must Have a Number.
-     */
-    public static boolean checkPassword(String password) throws UserRegistrationException{
-        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]?){8,}.*$").matcher(password).matches();
-        if (!check) {
-            return false;
-        }
-        else {
-            return true;
-        }
+    public static void main(String[] args) {
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.userValidate();
     }
 }
